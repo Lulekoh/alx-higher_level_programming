a68e45a380ba8a4d6813e392dec6f57854897a7b
Lulekoh <lulekoleoquintus12@gmail.com>
2023-11-17 01:47:32 -0800

    1-rectangle.js, updated

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,9 @@
-ass Rectangle {
+#!/usr/bin/node
+class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 }
 
-module.exports = Rectangle;
+module.exports = Rectangle;;
