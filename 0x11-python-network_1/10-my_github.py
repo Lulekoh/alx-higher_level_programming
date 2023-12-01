85cfb82444ca14742d8b281ce3e315ad90d15835
Lulekoh <lulekoleoquintus12@gmail.com>
2023-12-01 15:27:13 -0800

    desplay my github id using api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
 """
-This script communicates with the github API commits endpoint
-and gets the sha and author name of the last ten commits
-"""
+import sys
 import requests
-from sys import argv
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    username: str = argv[1]
-    access_token: str = argv[2]
-
-    url: str = "https://api.github.com/user"
-
-    response = requests.get(url, auth=(username, access_token))
-    status: int = response.status_code
-
-    if status == 200:
-        user_id = response.json().get("id")
-        print(f"User ID: {user_id}")
-    else:
-        print(f"Failed to retrieve user ID. Status code: {status}")
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id")))
